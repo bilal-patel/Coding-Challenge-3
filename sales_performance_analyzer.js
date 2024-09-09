09f8e83688119cdd17742ba7d6ba2799db6f4b69
Bilal Patel <bilalpatel@outlook.com>
2024-09-08 21:58:10 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -15,4 +15,18 @@ return averageSales;
 let sales = [450, 300, 500, 500]
 console.log(calculateAverageSales(sales));
 
+// Task 2 - Create a Function to Determine Performance Rating
 
+function determinePerformanceRating (averageSales) {
+    if (averageSales > 10000) {
+        return "Excellent";
+    } else if (averageSales >= 7000 && averageSales <= 10000) {
+        return "Good";
+    } else if (averageSales >= 4000 && averageSales < 7000) {
+        return "Satisfactory";
+    } else if (averageSales < 4000) {
+        return "Needs Improvement";
+    }
+}
+let averageSales = 3000
+console.log(determinePerformanceRating(averageSales))
